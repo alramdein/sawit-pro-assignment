d6a77e4bac8be22a3d236debd740f0cb19751b7c
alramdein <ramdanialif26@gmail.com>
2024-03-14 23:22:57 +0700

    misc: fix initial unit test

--- handler/auth.go
+++ handler/auth.go
@@ -7,13 +7,13 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-func (s *Server) Register(ctx echo.Context, in generated.RegisterJSONBody) error {
+func (s *Server) Register(ctx echo.Context) error {
 	var resp generated.RegisterResponse
 
 	return ctx.JSON(http.StatusOK, resp)
 }
 
-func (s *Server) Login(ctx echo.Context, in generated.LoginJSONBody) error {
+func (s *Server) Login(ctx echo.Context) error {
 	var resp generated.LoginResponse
 
 	return ctx.JSON(http.StatusOK, resp)
--- handler/user.go
+++ handler/user.go
@@ -13,7 +13,7 @@ func (s *Server) GetProfile(ctx echo.Context) error {
 	return ctx.JSON(http.StatusOK, resp)
 }
 
-func (s *Server) UpdateProfile(ctx echo.Context, in generated.UpdateProfileJSONBody) error {
+func (s *Server) UpdateProfile(ctx echo.Context) error {
 	var resp generated.UpdateProfileResponse
 
 	return ctx.JSON(http.StatusOK, resp)
